a28881ee5c21b3d9b16265f70cc0ff9ce31f1ad5
7736243 <7736243@naver.com>
2021-08-03 23:26:40 +0900

    20210803 baekjoon 2753

---
+++ c++14.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    scanf("%d", &year);
+    
+    if(year%4==0){
+        if(year%100!=0 || year%400==0){
+            printf("1");
+            return 0;
+        }
+    }
+    
+    printf("0");
+    return 0;
+}
